2681411bfd667b206cab5a0dd4a4bee36578bfcb
Matthieu Dreher <dreher.matthieu@gmail.com>
2024-11-01 21:36:21 -0400

    Upgrade package version to avoid duplicated version on test pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="lammpsinputbuilder",
-    version='0.0.2',
+    version='0.0.3',
     packages=find_packages(where='python'),
     package_dir={'': 'python'},
     package_data={"lammpsinputbuilder": ["lib_units.txt"]},
